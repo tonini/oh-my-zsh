04c3add05bf6ac975cb58984edbfcee80a512d3a
Samuel Tonini <tonini.samuel@gmail.com>
2011-01-17 18:11:04 +0100

    customize aliases

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -1,23 +1,37 @@
-# Push and pop directories on directory stack
-alias pu='pushd'
-alias po='popd'
+# cd
+alias ..='cd ..'
 
-# Basic directory operations
-alias ...='cd ../..'
-alias -- -='cd -'
+# ls
+alias ls="ls -F"
+alias l="ls -lAh"
+alias ll="ls -l"
+alias la='ls -A'
 
-# Super user
-alias _='sudo'
+# git
+alias gl='git log'
+alias glg='git log --graph --oneline'
+alias gpl='git pull --rebase'
+alias gps='git push'
+alias gd='git diff'
+alias gc='git commit'
+alias gca='git commit -a'
+alias gco='git checkout'
+alias gb='git branch'
+alias gs='git status'
+alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias changelog='git log `git log -1 --format=%H -- CHANGELOG*`..; cat CHANGELOG*'
 
-#alias g='grep -in'
+# rails
+alias sc='script/console'
+alias ss='script/server'
+alias sg='script/generate'
+alias tlog='tail -f log/development.log'
+alias migrate='rake db:migrate db:test:clone'
+alias rc='rails c'
+alias ras='rails s'
+alias rg='rails g'
+alias it="echo '\e[44mStarting Infinity Test with: --rspec\e[0m' &&infinity_test --rspec"
+alias cpw='cucumber -p wip'
 
 # Show history
 alias history='fc -l 1'
-
-# List direcory contents
-alias lsa='ls -lah'
-alias l='ls -la'
-alias ll='ls -l'
-alias sl=ls # often screw this up
-
-alias afind='ack-grep -il'
